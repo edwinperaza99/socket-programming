de1dbfefd504851f286638d7ac9bedc4a92ea2a3
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2023-09-21 17:07:33 -0700

    added error handling and closing messages

--- Programming-Assignment-1/client.py
+++ Programming-Assignment-1/client.py
@@ -1,14 +1,31 @@
 import socket 
 
-# ask for IP address and port number from user
-serverPort = int(input("Enter server port number: "))
+# ask for port number to connect to and check that it is in the range of 10,000 to 20,000
+while True:
+    try:
+        serverPort = int(input("Enter server port number: "))
+        if serverPort >= 10000 and serverPort <= 20000:
+            break
+        else:
+            print("Port number must be between 10,000 and 20,000.")
+    except ValueError:
+        print("Invalid input. Port number must be an integer.")
+
+# ask for IP address to connect to
 serverIP = input("Enter server IP address: ")
 
 # create a client socket 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-clientSocket.connect((serverIP, serverPort))
-print("Client has connected to the server!")
+# connect to the server
+try:
+    clientSocket.connect((serverIP, serverPort))
+except socket.error as errorMessage:
+    print(f"Failed to connect to client. Error: {errorMessage}")
+    clientSocket.close()
+    exit(1)
+
+print("\nClient has connected to the server!\n")
 
 while True:
     message = input("Enter a message to send to server: ")
@@ -16,11 +33,22 @@ while True:
     if message == "quit":
         clientSocket.send(message.encode())
         print("Client informing server that it is quitting.")
-        messages = clientSocket.recv(2048).decode()
-        print("Client received messages log")
-        print(messages)
-        break
+        try:
+            messages = clientSocket.recv(2048).decode()
+            if not messages:
+                print("No messages were received from server messages log.")
+                break
+            print("Client received messages log")
+            print(messages)
+            break
+        except socket.error as errorMessage:
+            print(f"Failure in receiving messages log. Error: {errorMessage}")
+            clientSocket.close()
+            exit(1)
     else:
         clientSocket.send(message.encode())
 
 clientSocket.close()
+
+# message that program has finished
+print("\nClient has closed the connection to the server.\n")
--- Programming-Assignment-1/server.py
+++ Programming-Assignment-1/server.py
@@ -3,14 +3,26 @@ import socket
 # array to store messages from client 
 messages = []
 
-# ask for port number from user
-serverPort = int(input("Enter server port number: "))
+# ask for port number to connect to and check that it is in the range of 10,000 to 20,000
+while True:
+    try:
+        serverPort = int(input("Enter server port number: "))
+        if serverPort >= 10000 and serverPort <= 20000:
+            break
+        else:
+            print("Port number must be between 10,000 and 20,000.")
+    except ValueError:
+        print("Invalid input. Port number must be an integer.")
 
 # create a server socket 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # bind the port number to the server socket 
-serverSocket.bind(('', serverPort))
+try:
+    serverSocket.bind(('', serverPort))
+except socket.error as errorMessage:
+    print(f"Failed to bind port number. Error: {errorMessage}")
+    exit(1)
 
 # Get the hostname and IP address of the server
 hostname = socket.gethostname()
@@ -19,27 +31,48 @@ serverAddress = socket.gethostbyname(hostname)
 # Print server details
 print("Server hostname is:", hostname)
 print("Server is at address:", serverAddress)
-print("Server is using port:", serverPort)
+print("Server is using port:", serverPort, end="\n\n")
 
 # wait for a connection to the server 
-serverSocket.listen(1)
+try:
+    serverSocket.listen(1)
+except socket.error as errorMessage:
+    print(f"Failure while waiting for connection. Error: {errorMessage}")
+    serverSocket.close()
+    exit(1)
 
 # accept the connection from the client 
-connection, clientAddress = serverSocket.accept()
-print("Server connected to client at address:", clientAddress)
+try:
+    connection, clientAddress = serverSocket.accept()
+except socket.error as errorMessage:
+    print(f"Failure in accepting connection. Error: {errorMessage}")
+    serverSocket.close()
+    exit(1)
+
+
+print("Server connected to client at address:", clientAddress, end="\n\n")
 
 while True:
-    message = connection.recv(2048).decode()
-    # check if the message is quit, if not just continue with loop
-    if message == "quit":
-        print("The client has informed the server that it is quitting.")
-        # with join we separate the messages with a new line
-        connection.send("\n".join(messages).encode())
-        break
-    else:
-        print("Server received message:", message)
-        messages.append(message)
+    try:
+        message = connection.recv(2048).decode()
+        # check if the message is quit, if not just continue with loop
+        if message == "quit":
+            print("\nThe client has informed the server that it is quitting.")
+            # with join we separate the messages with a new line and we send messages to client
+            connection.send("\n".join(messages).encode())
+            break
+        else:
+            print("Server received message:", message)
+            # store message in log 
+            messages.append(message)
+    except socket.error as errorMessage:
+        print(f"Failure in message reception. Error: {errorMessage}")
+        serverSocket.close()
+        exit(1)
 
 # close the connection between client and server
 connection.close()
 serverSocket.close()
+
+# message that program has finished
+print("\nServer has closed the connection with the client.\n")
