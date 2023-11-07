f2c1aff75448c873973437f8e484b61aaecc3905
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2023-11-07 01:59:30 -0800

    change variable name

--- Assignment-3/many-to-many.py
+++ Assignment-3/many-to-many.py
@@ -29,10 +29,10 @@ def set_alias(host_ip, alias):
     """Sets the alias for a connection."""
     global SOCKETS_LIST
     alias_found = False
-    for socket in SOCKETS_LIST:
-        if socket['alias'] == host_ip or socket['address'][0] == host_ip:
-            socket['alias'] = alias
-            print(f"Alias for {socket['address'][0]} set to {socket['alias']}")
+    for socket_info in SOCKETS_LIST:
+        if socket_info['alias'] == host_ip or socket_info['address'][0] == host_ip:
+            socket_info['alias'] = alias
+            print(f"Alias for {socket_info['address'][0]} set to {socket_info['alias']}")
             alias_found = True
             break
     if alias_found == False:
@@ -69,7 +69,7 @@ def connect(serverIP, serverPort):
     # connect to the server
     try:
         chat_connection.connect((serverIP, serverPort))
-        print(f"\nSuccessfully CONNECTED with {serverIP}\n")
+        print(f"\nSuccessfully connected with {serverIP}\n")
         # Prompt the user for an alias
         while True:
             response = input("Do you want to set an alias? (y/n): ").strip().lower()
@@ -82,7 +82,7 @@ def connect(serverIP, serverPort):
                 # TODO: might need to try with "pass"
                 continue
             # Check if the alias already exists in SOCKETS_LIST
-            alias_exists = any(socket['alias'] == alias for socket in SOCKETS_LIST)
+            alias_exists = any(socket_info['alias'] == alias for socket_info in SOCKETS_LIST)
             if not alias_exists:
                 break  # Exit the loop if the alias is unique
             print(f"Alias '{alias}' is already in use. Please choose a different alias.")
@@ -99,11 +99,11 @@ def disconnect(alias):
     """Disconnects from the chat server."""
     global CONNECTED, SOCKETS_LIST
     alias_found = False
-    for socket in SOCKETS_LIST:
-        if socket['alias'] == alias or socket['address'][0] == alias:
-            print(f"Disconnected from {socket['alias']}\n")
-            socket['socket'].close()
-            SOCKETS_LIST.remove(socket)
+    for socket_info in SOCKETS_LIST:
+        if socket_info['alias'] == alias or socket_info['address'][0] == alias:
+            print(f"Disconnected from {socket_info['alias']}\n")
+            socket_info['socket'].close()
+            SOCKETS_LIST.remove(socket_info)
             alias_found = True
             break
     if alias_found == False:
@@ -117,16 +117,16 @@ def send(message, alias):
     global SOCKETS_LIST
     alias_found = False
     if CONNECTED == True:
-        for socket in SOCKETS_LIST:
-            if socket['alias'] == alias or socket['address'][0] == alias:
-                socket['socket'].send(message.encode())
-                print(f"Message '{message}' sent to {socket['alias']}")
+        for socket_info in SOCKETS_LIST:
+            if socket_info['alias'] == alias or socket_info['address'][0] == alias:
+                socket_info['socket'].send(message.encode())
+                print(f"Message '{message}' sent to {socket_info['alias']}")
                 alias_found = True
                 break
         if alias_found == False:
             print(f"Alias '{alias}' not found. Please try again.")
     else: 
-        print("\nNot CONNECTED to a chat. Please connect to a chat before sending a message.")
+        print("\nNot connected to a chat. Please connect to a chat before sending a message.")
 
 
 def receive_message():
@@ -134,30 +134,28 @@ def receive_message():
     global CONNECTED, SOCKETS_LIST, EXIT_FLAG
     while not EXIT_FLAG:
         if CONNECTED == True:
-            for socket in SOCKETS_LIST:
+            for socket_info in SOCKETS_LIST:
                 # TODO: MIGHT NEED TO REMOVE THIS TRY AND EXCEPT 
                 try:
-                    message = socket['socket'].recv(1024).decode()
+                    message = socket_info['socket'].recv(1024).decode()
                     if not message:
-                        print(f"Disconnected from {socket['alias']}\n")
-                        socket['socket'].close()
-                        SOCKETS_LIST.remove(socket)
+                        print(f"Disconnected from {socket_info['alias']}\n")
+                        socket_info['socket'].close()
+                        SOCKETS_LIST.remove(socket_info)
                         if len(SOCKETS_LIST) == 0:
                             CONNECTED = False
                     if message:
-                        print(f"{socket['alias']}: {message}")
-                except socket.error as errorMessage:
-                    # TODO: MIGHT WANT TO REMOVE THIS AND JUST CLOSE SOCKET INSTEAD 
-                    # print(f"Failed to receive message. Error: {errorMessage}")
-                    # exit(1)
-                    socket['socket'].close()
-                    SOCKETS_LIST.remove(socket)
+                        print(f"\n{socket_info['alias']}: {message}\n\nEnter command: ", end="")
+                except (ConnectionResetError, ConnectionAbortedError) as errorMessage:
+                    print(f"Disconnected from {socket_info['alias']}\n")
+                    socket_info['socket'].close()
+                    SOCKETS_LIST.remove(socket_info)
                     if len(SOCKETS_LIST) == 0:
                         CONNECTED = False
-                except (ConnectionResetError, ConnectionAbortedError) as errorMessage:
-                    print(f"Disconnected from {socket['alias']}\n")
-                    socket['socket'].close()
-                    SOCKETS_LIST.remove(socket)
+                except socket.error as errorMessage:
+                    # TODO: MIGHT WANT TO REMOVE THIS AND JUST CLOSE SOCKET INSTEAD 
+                    socket_info['socket'].close()
+                    SOCKETS_LIST.remove(socket_info)
                     if len(SOCKETS_LIST) == 0:
                         CONNECTED = False
         else:
@@ -282,8 +280,8 @@ def main():
                 EXIT_FLAG = True       # set exit flag to true
                 server_socket.close()  # close server socket
                 # close all sockets 
-                for socket in SOCKETS_LIST:
-                    socket['socket'].close()
+                for socket_info in SOCKETS_LIST:
+                    socket_info['socket'].close()
                 server_thread.join()    # wait for server thread to finish
                 receive_thread.join()   # wait for receive thread to finish
                 exit(0)                 # exit program gracefully
